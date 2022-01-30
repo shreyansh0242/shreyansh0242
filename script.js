44378d7afb51c13d85f67accdf1b8c5a194f9cc8
Shreyansh <shreyanshagss@gmail.com>
2022-01-30 12:02:05 +0530

    updated

--- script.js
+++ script.js
@@ -36,10 +36,5 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
-    var typed = new Typed(".typing-2", {
-        strings: ["NSUTian","learner","Creator", "Designer","Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    
 });
